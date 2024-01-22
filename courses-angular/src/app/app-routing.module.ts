b3fc47c3594bdc6fe1e6e616254500a0d8a885ce
ShailaAshraf <110811156+ShailaAsharaf@users.noreply.github.com>
2024-01-22 12:20:19 +0530

    commit2

--- courses-angular/src/app/app-routing.module.ts
+++ courses-angular/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dash/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dash',
+    component: LayoutComponent,
+    data: {
+      title: 'Navigation'
+    },
+    children: [
+      {
+        path:'home',
+        component: DashboardComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'shopping-cart',
+        component: ShoppingCartComponent
+      },
+      {
+        path: 'wishlist',
+        component: WishlistComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- courses-angular/src/app/app.module.ts
+++ courses-angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    ShoppingCartComponent,
+    CourseDetailsComponent,
+    WishlistComponent,
+    ProfileComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
