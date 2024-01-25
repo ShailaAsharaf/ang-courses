1ef00d98b4ed971ec4f04e8050ca29337adf089f
ShailaAshraf <110811156+ShailaAsharaf@users.noreply.github.com>
2024-01-25 12:10:22 +0530

    coursedetails commit1

--- courses-angular/src/app/components/course-details/course-details.component.ts
+++ courses-angular/src/app/components/course-details/course-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { courses } from 'src/app/data-model';
+import { DataServiceService } from '../data-service.service';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupComponent } from '../popup/popup.component';
 
 @Component({
   selector: 'app-course-details',
@@ -10,14 +13,17 @@ import { courses } from 'src/app/data-model';
 export class CourseDetailsComponent implements OnInit {
   courseId: string|null = '';
   course!: courses;
+  isVideoPlaying = false;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public dataServices: DataServiceService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.courseId = this.route.snapshot.paramMap.get('courseId');
-    const findIndex = courses.ConstValue.findIndex((a: any)=>{
+    courses.ConstValue.findIndex((a: any)=>{
       let cName = a.courseName.replace(/\s/g, "").toLowerCase();
       if(cName == this.courseId){
         this.course = a;
@@ -27,4 +33,46 @@ export class CourseDetailsComponent implements OnInit {
 
   }
 
+  playVideo() {
+    this.isVideoPlaying = !this.isVideoPlaying;
+  }
+  addToWishList(item: courses) {
+    const daialogRef = this.dialog.open(PopupComponent, {
+      width: '300px',
+      data: {
+        mode: 'addedToWishList',
+      },
+    });
+    daialogRef.afterClosed().subscribe(resp=>{
+      this.dataServices.wishItems.push(item);
+    })
+  }
+  addToCart(item: courses) {
+    if (this.dataServices.cartItems.indexOf(item) !== -1){
+      alert('Already exists in the Cart')
+    } else{
+      const daialogRef = this.dialog.open(PopupComponent, {
+        width: '300px',
+        data: {
+          mode: 'addedToCart',
+        },
+      });
+      daialogRef.afterClosed().subscribe(resp=>{
+        this.dataServices.cartItems.push(item)
+      })
+    }
+  }
+  checkElement(element: courses){
+    if (this.dataServices.wishItems.indexOf(element) !== -1){ return true;}
+    else{ return false; }
+  }
+  getPrice(price: string, discount: string) {
+    let finalPrice = price.substring(1);
+    let finalDis = discount.substring(0, discount.length - 1);
+    let actualPrice: number = +finalPrice;
+    let discountPercentage: number = +finalDis;
+    let discountedPrice = actualPrice * (1 - discountPercentage / 100);
+    let lastPrice = (Math.round(discountedPrice * 100) / 100).toFixed(2);
+    return lastPrice;
+  }
 }
